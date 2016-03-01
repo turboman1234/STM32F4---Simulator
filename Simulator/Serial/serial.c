3d1a56710d4c2aae256c819350dfbfcc4847fcc4
Stanislav Kitov <s_kitov@tu-sofia.bg>
2016-03-01 13:07:39 +0200

    fix bug it is needed control for buffer size, because processor will go in hardfault handler

--- Simulator/Serial/serial.c
+++ Simulator/Serial/serial.c
@@ -2,6 +2,7 @@
 #include "definitions.h"
 #include "VTimer.h"
 #include "usart.h"
+#include "mbmaster.h"
 #include "serial.h"
 
 
@@ -15,26 +16,32 @@ unsigned char GetByte(int usartID)
     return recieveMyUSART(usartID);
 }
 
-//char *Str - pointer to data which will be send
+//char *Str - pointer to recieved buffer
 //int len - number of sending data bytes
 //int usartID - USART_2 for ModBus and USART_3 fot serial communication RS232
 //int timerType - TIMER_XX, MB_MASTER_TIMER, TIMER_COMMUNICATION ...
-//int miliseconds - timeout between frames sending - T_10_MS, T_100_MS, T_1_S...
+//int miliseconds - timeout between frames recieving - T_10_MS, T_100_MS, T_1_S...
 int InString(unsigned char *Str, int usartID, int timerType, int miliseconds)
 {   
-    int x, isTimerElapsed;
+    int isTimerElapsed;
     int BytesReceived = 0;
+    unsigned char x;
     
     SetVTimerValue(timerType, miliseconds);
     while(IsVTimerElapsed(timerType) == FALSE)
     {		
         x = recieveMyUSART(usartID);		
         isTimerElapsed = IsVTimerElapsed(timerType);
-        if(x != -1 && isTimerElapsed == FALSE)
+        if((signed char)x != -1 && isTimerElapsed == FALSE)
         {
             SetVTimerValue(timerType, miliseconds);
             Str[BytesReceived] = x;
             BytesReceived ++;
+            if (BytesReceived == RESPONSE_MAX_SIZE)
+            {
+                // recieve buffer is full
+                break;
+            }
         }
     }
     return BytesReceived;
